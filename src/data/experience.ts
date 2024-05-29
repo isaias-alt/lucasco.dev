9f6a3095adcabbd5b18d1f2a507a56eae68638ef
isaias-at <cascolucasisaias@gmail.com>
2024-05-29 15:47:17 -0400

    ✅ add projects

---
+++ src/data/experience.ts
@@ -0,0 +1,15 @@
+interface Experience {
+  role: string
+  aboutRole: string
+  company?: string
+  companyUrl?: string
+  date: string
+}
+
+export const EXPERERIENCE: Experience[] = [
+  {
+    role: 'Freelancer',
+    aboutRole: 'For the last few years, I have worked as a freelance frontend developer, specializing in creating frontend technologies that allow companies and businesses to showcase and market their products with exceptional effectiveness.',
+    date: '2021 - Present',
+  },
+]
---
+++ src/data/projects.ts
@@ -0,0 +1,48 @@
+interface Projects {
+  title: string
+  description: string
+  tags?: string[]
+  url?: string
+  githubUrl?: string
+  projectIcon?: string
+  image: string
+}
+
+export const PROJECTS: Projects[] = [
+  {
+    title: 'Link.ucsi',
+    description: 'Est consectetur pariatur cillum voluptate. Est elit tempor Lorem proident elit consectetur magna mollit occaecat esse minim consectetur.',
+    tags: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    url: 'https://github.com/isaias-alt',
+    projectIcon: '[UC]',
+    githubUrl: 'https://github.com/isaias-alt',
+    image: '/projects/wikicinema.webp'
+  },
+  {
+    title: 'Link.ucsi',
+    description: 'Est consectetur pariatur cillum voluptate. Est elit tempor Lorem proident elit consectetur magna mollit occaecat esse minim consectetur.',
+    tags: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    url: 'https://github.com/isaias-alt',
+    projectIcon: '',
+    githubUrl: 'https://github.com/isaias-alt',
+    image: '/projects/wikicinema.webp'
+  },
+  {
+    title: 'Link.ucsi',
+    description: 'Est consectetur pariatur cillum voluptate. Est elit tempor Lorem proident elit consectetur magna mollit occaecat esse minim consectetur.',
+    tags: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    url: 'https://github.com/isaias-alt',
+    projectIcon: '',
+    githubUrl: 'https://github.com/isaias-alt',
+    image: '/projects/wikicinema.webp'
+  },
+  {
+    title: 'Link.ucsi',
+    description: 'Est consectetur pariatur cillum voluptate. Est elit tempor Lorem proident elit consectetur magna mollit occaecat esse minim consectetur.',
+    tags: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    url: 'https://github.com/isaias-alt',
+    projectIcon: '',
+    githubUrl: 'https://github.com/isaias-alt',
+    image: '/projects/wikicinema.webp'
+  },
+]
