7a4f6fe331b94cd6b25a3156d4454adc50608331
isaias-at <cascolucasisaias@gmail.com>
2024-06-13 02:19:43 -0400

    Add About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto p-8">
       <Header />
+      <About />
     </main>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <article className="pt-8 opacity-80">
+      <p className="leading-loose">
+        I'm <span className="font-bold tracking-tight">Lucas</span>, FrontEnd Developer from Paraguay with <span className="font-bold">+3</span> years of experience. 
+        I'm passionate about building user-friendly interfaces and open-source software.
+      </p>
+    </article>
+  )
+}
+
+export default About
