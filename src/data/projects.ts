e516a11aa918691c8c348d42bd2860b5a56ebde9
isaias-at <cascolucasisaias@gmail.com>
2024-07-02 19:09:37 -0400

    fix typo

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,7 +19,7 @@ export const PROJECTS: Project[] = [
   },
   {
     title: 'STI UCSI',
-    description: 'The website displays the Catholic University seminar Shows the speakers, their topics and the calendar of events',
+    description: 'The website displays the Catholic University seminar. Shows the speakers, their topics and the calendar of events',
     tags: ['Atro', 'React.js', 'Tailwindcss'],
     url: 'https://sti-ucsi.vercel.app/',
     githubUrl: 'https://github.com/isaias-alt/sti-ucsi',
