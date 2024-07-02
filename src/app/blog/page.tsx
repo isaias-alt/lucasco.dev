e57b412b8f812a6cb5a9684607b13439da2b1e25
isaias-at <cascolucasisaias@gmail.com>
2024-07-02 13:18:35 -0400

    chage font weight

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,7 @@ const Blog = () => {
   return (
     <main className="max-w-4xl mx-auto p-8">
       <Header />
-      <h1 className="text-3xl font-bold mb-4 py-8">All Posts</h1>
+      <h1 className="text-xl font-medium mb-4 py-6">All Posts</h1>
       <div className="flex flex-col space-y-4">
         {allPostsData.map(({ id, title, description, date }) => (
           <PostCard key={id} title={title} description={description} date={date} slug={id} />
