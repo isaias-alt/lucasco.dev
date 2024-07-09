bce0d5a77bd78bbfe91011c6dcb077d597f52009
isaias-at <cascolucasisaias@gmail.com>
2024-07-09 00:26:53 -0400

    delete unnecessary events and dependecies

--- src/hooks/useProjectCardHover.ts
+++ src/hooks/useProjectCardHover.ts
@@ -2,28 +2,17 @@ import { useRef, useState, useEffect, useCallback } from "react";
 
 const useProjectCardHover = () => {
   const divRef = useRef<HTMLDivElement>(null);
-  const [focused, setFocused] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [opacity, setOpacity] = useState(0);
 
   const handleMouseMove = useCallback((event: MouseEvent) => {
-    if (!divRef.current || focused) return;
+    if (!divRef.current) return;
     const rect = divRef.current.getBoundingClientRect();
     setPosition({
       x: event.clientX - rect.left,
       y: event.clientY - rect.top,
     });
-  }, [focused]);
-
-  const handleFocus = () => {
-    setFocused(true);
-    setOpacity(1);
-  };
-
-  const handleBlur = () => {
-    setFocused(false);
-    setOpacity(0);
-  };
+  }, []);
 
   const handleMouseEnter = () => {
     setOpacity(1);
@@ -37,20 +26,16 @@ const useProjectCardHover = () => {
     const divElement = divRef.current;
     if (divElement) {
       divElement.addEventListener("mousemove", handleMouseMove);
-      divElement.addEventListener("focus", handleFocus);
-      divElement.addEventListener("blur", handleBlur);
       divElement.addEventListener("mouseenter", handleMouseEnter);
       divElement.addEventListener("mouseleave", handleMouseLeave);
 
       return () => {
         divElement.removeEventListener("mousemove", handleMouseMove);
-        divElement.removeEventListener("focus", handleFocus);
-        divElement.removeEventListener("blur", handleBlur);
         divElement.removeEventListener("mouseenter", handleMouseEnter);
         divElement.removeEventListener("mouseleave", handleMouseLeave);
       };
     }
-  }, [focused, handleMouseMove])
+  }, [handleMouseMove])
 
   return {
     divRef,
