1d837c5c9a04c1feebadf62bc06d4030aee9371b
isaias-at <cascolucasisaias@gmail.com>
2024-07-13 16:55:46 -0400

    improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,36 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lucas Casco - Personal website",
-  description:
-    "Frontend Developer specialized in creating unique and amazing applications.",
+  description: "Frontend Developer specialized in creating unique and amazing applications.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://lucascasco.com",
+    title: "Lucas Casco - Personal website",
+    description: "Frontend Developer specialized in creating unique and amazing applications.",
+    images: [
+      {
+        url: "https://lucascasco.com/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Lucas Casco - Personal website",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lucas Casco - Personal website",
+    creator: "@lucascascodev",
+    description: "Frontend Developer specialized in creating unique and amazing applications.",
+    images: [
+      {
+        url: "https://lucascasco.com/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Lucas Casco - Personal website",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
