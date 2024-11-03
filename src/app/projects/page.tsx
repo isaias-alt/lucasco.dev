76d1cd4c4705ac11b844e62ea7e4d61ae409a462
isaias-alt <cascolucasisaias@gmail.com>
2024-11-03 14:18:19 -0300

    add projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,52 @@
+import BlurFade from "@/components/magicui/blur-fade"
+import { ProjectCard } from "@/components/project-card"
+import { DATA } from "@/data/resume"
+
+const BLUR_FADE_DELAY = 0.04
+
+const ProjectPage = () => {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section>
+        <div className="space-y-12 w-full py-8">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                My Projects
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                I like to build things
+              </h2>
+              <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                I&apos;ve built a variety of projects ranging from web apps, open source projects and more. Here are a few of my favorites.
+              </p>
+
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default ProjectPage
