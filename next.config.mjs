5b092ace22c78558c2b560eb18a445375604fa76
isaias-alt <cascolucasisaias@gmail.com>
2024-11-11 18:11:23 -0300

    delete redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-import redirects from "./src/config/redirects.mjs";
 
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return redirects;
-  },
 };
 
 export default nextConfig;
